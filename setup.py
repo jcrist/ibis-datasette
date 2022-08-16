63ba9e587dc96f617c39125b2588a5f0d3c3a90a
Jim Crist-Harif <jcristharif@gmail.com>
2022-08-16 15:56:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,18 @@ setup(
     ),
     maintainer="Jim Crist-Harif",
     maintainer_email="jcristharif@gmail.com",
+    url="https://github.com/jcrist/ibis-datasette",
+    project_urls={
+        "Source": "https://github.com/jcrist/ibis-datasette/",
+        "Issue Tracker": "https://github.com/jcrist/ibis-datasette/issues",
+    },
+    keywords="ibis datasette pandas sqlite",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
     license="BSD",
     packages=["ibis_datasette"],
     python_requires=">=3.8",
